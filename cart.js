6e6fe64b991a4581547a998129c76524c4ffd846
jackycth2024 <chantszho2014@gmail.com>
2024-03-10 13:57:37 +0800

    Update cart.js

--- cart.js
+++ cart.js
@@ -22,7 +22,7 @@ function updateShoppingList() {
         var productInfo = document.createElement("span");
         var quantities = document.createElement("span");
         productInfo.innerHTML = item.name + " - $" + item.price.toFixed(2) + " x " + getTotalQuantity(itemName);
-        quantities.innerHTML = "<input type='number' class='quantity-input' placeholder='" + getTotalQuantity(itemName) + "' oninput='updateQuantity(\"" + itemName + "\", this.value)'></input>";
+        quantities.innerHTML = "<input type='number' class='quantity-input' placeholder='" + getTotalQuantity(itemName) + "' onblur='updateQuantity(\"" + itemName + "\", this.value)'></input>";
         quantities.innerHTML += "<button class='removeBtn' onclick='removeproduct(\"" + itemName + "\")'>Remove</button>";
         listItem.append(productInfo,quantities);       
         shoppingListContainer.appendChild(listItem);
@@ -59,6 +59,7 @@ function getTotalQuantity(itemName) {
 
 function updateQuantity(itemName, newQuantity) {
     cartItems[itemName].quantity = newQuantity;
+    updateShoppingList();
 }
 
 
