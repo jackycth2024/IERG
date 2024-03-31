3f2851496c24ef49f206b282c72fd94af38d0e56
jackycth2024 <chantszho2014@gmail.com>
2024-03-31 18:40:52 +0800

    add /api/

--- admin.js
+++ admin.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function fetchCategories() {
-        fetch('/categories')
+        fetch('/api/categories')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
