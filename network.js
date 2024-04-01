e52a01b85b8465e147d71c2526bd75c95c70d6b4
jackycth2024 <chantszho2014@gmail.com>
2024-04-01 22:10:15 +0800

    Update network.js

--- network.js
+++ network.js
@@ -1,5 +1,5 @@
 function fetchCategories() {
-    fetch('/api/categories')
+    fetch('/categories')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
