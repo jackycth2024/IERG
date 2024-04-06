cfe280bcf0956ed4ff119ef475868bdc45576940
jackycth2024 <chantszho2014@gmail.com>
2024-04-06 18:55:16 +0800

    .

--- checkout.js
+++ checkout.js
@@ -1,10 +1,10 @@
 var shoppingCart = document.getElementById("cart");
-var shoppingList = document.getElementById("shoppingList");
+var checkoutList = document.getElementById("checkoutlist");
 var cartItems = JSON.parse(localStorage.getItem("cartItems")) || {};
 var quantityInputs = {};
 
 function updateShoppingList() {
-    shoppingList.innerHTML = "";
+    checkoutList.innerHTML = "";
     var totalAmount = 0;
 
     for (var itemName in cartItems) {
@@ -19,7 +19,7 @@ function updateShoppingList() {
         quantities.innerHTML = "<input type='number' class='quantity-input' placeholder='" + getTotalQuantity(itemName) + "' min='1' pattern='^[1-9]\d*$*' onblur='updateQuantity(\"" + itemName + "\", this.value)'></input>";
         quantities.innerHTML += "<button class='removeBtn' onclick='removeproduct(\"" + itemName + "\")'>Remove</button>";
         listItem.append(productInfo,quantities);       
-        shoppingList.appendChild(listItem);
+        checkoutList.appendChild(listItem);
 
         // Calculate subtotal for the current item and add it to total amount
         var subtotal = item.price * getTotalQuantity(itemName);
@@ -29,30 +29,22 @@ function updateShoppingList() {
 
     var line = document.createElement("hr");
     line.className = "separator-line";
-    shoppingList.appendChild(line);
+    checkoutList.appendChild(line);
 
     var totalAmountContainer = document.createElement("div");
     totalAmountContainer.textContent = "Total Amount: $" + totalAmount.toFixed(2);
-    shoppingList.appendChild(totalAmountContainer);
+    checkoutList.appendChild(totalAmountContainer);
 
     if (Object.keys(cartItems).length > 0) {
         // Checkout and clear button
-        shoppingList.innerHTML += "<button class='clearBtn' onclick='clearShoppingCart()'>Clear</button>";
-        shoppingList.innerHTML += "<button class='checkoutBtn' onclick='tocheckout()'>Checkout</button>";
+        checkoutList.innerHTML += "<button class='clearBtn' onclick='clearShoppingCart()'>Clear</button>";
+        checkoutList.innerHTML += "<button class='checkoutBtn' onclick='checkout()'>Checkout</button>";
     } else {
-        shoppingList.innerHTML = "<p>Empty</p>";
+        checkoutList.innerHTML = "<p>Empty</p>";
     }
 
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
 }
-
-//function copyShoppingList() {
-//    updateShoppingList();
-//    const shoppingListBody = document.getElementById('shoppingListBody');
-//    shoppingListBody.innerHTML = '';
-//    const clonedShoppingList = shoppingList.cloneNode(true);
-//    shoppingListBody.appendChild(clonedShoppingList);
-//}
 
 function getTotalQuantity(itemName) {
     var item = cartItems[itemName];
@@ -99,32 +91,8 @@ function clearShoppingCart() {
     updateShoppingList();
 }
 
-function tocheckout() {
-    window.location.href = '/checkout.html';
-}
-
 function checkout() {
    cartItems = {};
    localStorage.setItem("cartItems", JSON.stringify(cartItems));
    updateShoppingList();
 }
-
-shoppingCart.addEventListener("mouseover", function(event) {
-    updateShoppingList();
-    shoppingCart.addEventListener("mouseover", function(event) {
-        // Get mouse coordinates
-        var mouseX = event.clientX;
-        var mouseY = event.clientY;
-
-        // Set the shopping list position
-        shoppingList.style.display = "block";
-        shoppingList.style.position = "fixed";
-        shoppingList.style.top = mouseY + "px";
-        shoppingList.style.left = mouseX + "px";
-        updateShoppingList();
-    });
-});
-
-shoppingList.addEventListener("mouseleave", function() {
-    shoppingList.style.display = "none";
-});
