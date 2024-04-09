6c9fbd8f64201086a4f45e247a226891178f504c
jackycth2024 <chantszho2014@gmail.com>
2024-04-09 23:35:37 +0800

    .

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ function showLoggedInUser(username) {
 
     const accountName = document.createElement('span');
     accountName.textContent = username;
-    logoutButton.addEventListener('click', window.location.href = '/memberportal.html');
+    accountName.addEventListener('click', window.location.href = '/memberportal.html');
     accountContainer.appendChild(accountName);
 
     accountContainer.appendChild(document.createTextNode('\u00A0'));
