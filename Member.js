1c2c4f7358b4b8de93f12c2229c4849ee029bcd6
jackycth2024 <chantszho2014@gmail.com>
2024-04-16 21:49:28 +0800

    Update Member.js

--- Member.js
+++ Member.js
@@ -38,6 +38,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     window.location.href = '/login.html';
                 }
             })
-            .catch(error => console.error('Error checking login status:', error));
+            .catch(error => {
+                console.error('Error checking login status:', error);
+                window.location.href = '/login.html';
+            });
     }
 });
