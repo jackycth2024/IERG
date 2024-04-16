ca5eaaf4be134e49f03a939958ac6123375c6764
jackycth2024 <chantszho2014@gmail.com>
2024-04-17 02:37:06 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -12,7 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
                     window.location.href = '/login.html';
                 }
             })
-            .catch(error => console.error('Error checking login status:', error));
+            .catch(error => {
+                console.error('Error checking login status:', error);
+                window.location.href = '/login.html';
+            });
     }
     const categoryForm = document.getElementById('categoryForm');
     const productForm = document.getElementById('productForm');
